6b12a7484add4db46ecf52454ccff62492d13c15
Iurii Kurinnoi <yur4kur@icloud.com>
2023-11-26 13:11:30 +0500

    Initial Commit

---
+++ Lesson-4.03_LoginField&Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lesson-4.03_LoginField&Timer
+//
+//  Created by Юрий Куринной on 26.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lesson-4.03_LoginField&Timer/Lesson_4_03_LoginField_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_03_LoginField_TimerApp.swift
+//  Lesson-4.03_LoginField&Timer
+//
+//  Created by Юрий Куринной on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_03_LoginField_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
